b625cf255116e3348f7f77629483c4a89e76a143
Prosenjit Majumder Joy <32308244+prosenjitjoy@users.noreply.github.com>
2023-06-09 00:45:02 +0600

    Update index.js

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ async function updateGist(stats) {
       gist_id: gistId,
       files: {
         [filename]: {
-          filename: `📊 Weekly development breakdown`,
+          filename: `📊 My Coding Stats`,
           content: lines.join("\n")
         }
       }
